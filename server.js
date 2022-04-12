e28174d9053341849a3f1b96e7ec2d7ecd8daca7
Marcio H <marcio.o@github.com>
2022-04-11 21:17:21 -0300

    integracao front-back p1

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const app = express()
 const path = require('path');
+const cors = require('cors')
 // const router = express.Router()
 
 require('dotenv').config()
+
+app.use(cors({
+    origin: `${process.env.BACKEND_URL}`
+}))
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views/pages'));
@@ -13,7 +18,7 @@ app.use(express.static(path.join(__dirname + '/public/')));
 app.get('/', (req, res) => {
     res.render("index", {
         title: 'DOIMZIM',
-        nav: ['Home', 'Sobre', 'Serviços', 'Contato']
+        menulist: ['Home', 'Sobre', 'Serviços', 'Contato']
     })
 })
 
@@ -40,9 +45,5 @@ app.get('/recupera-senha', (req, res) => {
         title: "Recuperar Senha"
     })
 })
-
-
-
-
 
 app.listen(process.env.PORT, () => console.log(`Server FrontEnd: http://localhost:${process.env.PORT}`))
