06f20045c47de408dbf5b02fd7d7b712fb3fb48e
Marcio H <marcio.o@github.com>
2022-04-12 23:19:26 -0300

    exclusao das rotas antigas no serverjs

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app = express()
 const path = require('path');
 const cors = require('cors')
-// const router = express.Router()
 
 const adminRoutes = require('./routes/adminRoutes')
 const loginRoutes = require('./routes/loginRoutes')
@@ -25,48 +24,6 @@ app.get('/', (req, res) => {
         menulist: ['Home', 'Sobre', 'Serviços', 'Contato']
     })
 })
-
-// app.get('/login', (req, res) => {
-//     res.render("login", {
-//         title: "Faça login"
-//     })
-// })
-
-// app.get('/esqueci-senha', (req, res) => {
-//     res.render("esqueci-senha", {
-//         title: "Esqueci Minha Senha"
-//     })
-// })
-
-// app.get('/registrar', (req, res) => {
-//     res.render("registrar", {
-//         title: "Registrar"
-//     })
-// })
-
-// app.get('/recupera-senha', (req, res) => {
-//     res.render("recupera-senha", {
-//         title: "Recuperar Senha"
-//     })
-// })
-
-// app.get('/dashboard', (req, res) => {
-//     res.render("dashboard", {
-//         title: "Dashboard"
-//     })
-// })
-
-// app.get('/dashboard_perfil', (req, res) => {
-//     res.render("dashboard_perfil", {
-//         title: "Perfil"
-//     })
-// })
-
-// app.get('/dashboard_pet', (req, res) => {
-//     res.render("dashboard_pet", {
-//         title: "Pet"
-//     })
-// })
 
 app.use('/login', loginRoutes)
 app.use('/dashboard', adminRoutes)
