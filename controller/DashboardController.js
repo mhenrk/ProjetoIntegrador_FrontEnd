f8360eb4dd9e944077184199d1fbdacb6a2427a4
Marcio H <marcio.o@github.com>
2022-04-25 00:29:11 -0300

    correcao de bugs, mensagens de erro

--- controller/DashboardController.js
+++ controller/DashboardController.js
@@ -3,43 +3,56 @@ const axios = require("axios")
 module.exports = {
 
     async index(req, res) {
-        // res.status(200).json({message: "Rota: DASHBOARD"})
         res.status(200).render("dashboard", {
-            title: "Dashboard | Anymals"
-            //terei que colocar mais controles no dash do admin
+            session: req.session
+            //por que tá vindo undefined?
         })
     },
 
     async showUserPets(req, res) {
 
-        const userID = req.session.userID
+        const { userID } = req.session
 
-        //precisa recupera o ID do usuário na sessão
-        //`${process.env.BACKEND_URL}/usuario/${userID}`
-        await axios.get(`${process.env.BACKEND_URL}/usuario/1`) 
+        await axios.get(`${process.env.BACKEND_URL}/usuario/${userID}`)
             .then(response => {
                 if (response.data.pet === null) return
 
-                res.status(200).render('dashboard_pet' , {
-                    title: 'Dashboard | Pet',
-                    listapet: response.data.pet
+                res.status(200).render('dashboard_pet', {
+                    pets: response.data.pet,
+                    session: req.session
                 })
-
             })
-            .catch(erro => console.log(`Ocorreu esse seguinte erro: ${erro}`))
+            .catch(erro => {
+                console.log(`Ocorreu esse seguinte erro: ${erro}`)
+                res.render('dashboard_pet', {
+                    message: 'Pet não encontrado / não cadastrado'
+                })
+            })
     },
 
     async showParceiros(req, res) {
 
         await axios.get(`${process.env.BACKEND_URL}/parceiros/`)
             .then(response => {
-                
-                res.status(200).render('dashboard_parceiro' , {
-                    title: 'Dashboard | ParceiroX',
-                    listaparceiro: response.data
+
+                res.status(200).render('dashboard_parceiro', {
+                    listaparceiro: response.data,
+                    session: req.session
                 })
 
             })
-            .catch(erro => console.log(`Ocorreu esse seguinte erro: ${erro}`))
+            .catch(erro => {
+                console.log(`Ocorreu esse seguinte erro: ${erro}`)
+                res.render('dashboard_parceiro', {
+                    message: 'Parceiro não encontrado / não cadastrado'
+                })
+            })
+    },
+
+    async profile(req, res) {
+        res.render('dashboard_perfil', {
+            redirect: '/perfil',
+            session: req.session
+        })
     }
 }
--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -4,63 +4,63 @@ const routes = express.Router()
 const dashboardController = require('../controller/DashboardController')
 
 /** /dashboard */
-routes.get('/', (req, res) => {
-    res.status(200).render('dashboard', {
-        title: 'Dashboard | DH Anymals'
-    })
+routes.get('/', dashboardController.index)
+
+routes.get('/perfil', dashboardController.profile)
+
+routes.get('/pet', dashboardController.showUserPets)
+
+routes.get('/parceiro', dashboardController.showParceiros)
+
+routes.get('/logout', (req, res) => {
+    res.session = ""
+    res.status(200).redirect('/')
 })
 
-routes.get('/admin', (req, res) => {
-    res.status(200).render('dashboard' , {
-        title: 'Dashboard Admin | DH Anymals'
-    })
-})
+
+// routes.get('/admin', (req, res) => {
+//     res.status(200).render('dashboard' , {
+//         title: 'Dashboard Admin | DH Anymals'
+//     })
+// })
 
 /** /dashboard/perfil */
-routes.get('/perfil', (req, res) => {
-    res.status(200).render('dashboard_perfil' , {
-        title: 'Dashboard Admin | Perfil'
-    })
-})
 
-routes.get('/admin/perfil', (req, res) => {
-    res.status(200).render('dashboard_perfil' , {
-        title: 'Dashboard Admin | Perfil'
-    })
-})
+
+// routes.get('/admin/perfil', (req, res) => {
+//     res.status(200).render('dashboard_perfil' , {
+//         title: 'Dashboard Admin | Perfil'
+//     })
+// })
 
 /** /dashboard/pet */
-routes.get('/pet', dashboardController.showUserPets)
+
 // routes.get('/pet', (req, res) => {
 //     res.status(200).render('dashboard_pet' , {
 //         title: 'Dashboard | Pet'
 //     })
 // })
 
-routes.get('/admin/pet', (req, res) => {
-    res.status(200).render('dashboard_pet' , {
-        title: 'Dashboard Admin | Pet'
-    })
-})
+// routes.get('/admin/pet', (req, res) => {
+//     res.status(200).render('dashboard_pet' , {
+//         title: 'Dashboard Admin | Pet'
+//     })
+// })
 
 /** /dashboard/parceiro */
-routes.get('/parceiro', dashboardController.showParceiros)
+
 // routes.get('/parceiro', (req, res) => {
 //     res.status(200).render('dashboard_parceiro' , {
 //         title: 'Dashboard | Parceiros'
 //     })
 // })
 
-routes.get('/admin/parceiro', (req, res) => {
-    res.status(200).render('dashboard_parceiro' , {
-        title: 'Dashboard Admin | Parceiros'
-    })
-})
+// routes.get('/admin/parceiro', (req, res) => {
+//     res.status(200).render('dashboard_parceiro' , {
+//         title: 'Dashboard Admin | Parceiros'
+//     })
+// })
 
 /** /dashboard/logout */
-routes.get('/logout', (req, res) => {
-    res.session = ""
-    res.status(200).redirect('/')
-})
 
 module.exports = routes
